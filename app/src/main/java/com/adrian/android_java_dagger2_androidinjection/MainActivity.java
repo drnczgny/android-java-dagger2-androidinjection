2d224260c501b3b165fef30ec214b8a91fd1c817
Adrian Czigany <c.adrian89@gmail.com>
2017-08-01 19:24:32 +0200

    init commit

---
+++ app/src/main/java/com/adrian/android_java_dagger2_androidinjection/MainActivity.java
@@ -0,0 +1,13 @@
+package com.adrian.android_java_dagger2_androidinjection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/adrian/android_java_dagger2_androidinjection/MyApplication.java
@@ -0,0 +1,10 @@
+package com.adrian.android_java_dagger2_androidinjection;
+
+import android.app.Application;
+
+/**
+ * Created by cadri on 2017. 08. 01..
+ */
+
+public class MyApplication extends Application {
+}
